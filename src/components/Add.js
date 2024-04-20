686a8cc0c20ce9b764431a823697c801e2a22c16
SHYNK <129217369+HibaElalami@users.noreply.github.com>
2024-04-20 02:05:32 +0100

    Update Add.js

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,24 +1,30 @@
 
 import { Link } from 'react-router-dom';
-import './Add.css'; // Assurez-vous que le chemin est correct pour votre fichier CSS
+import './Add.css';
 
 function Add() {
- 
   return (
-  
-    <form >
-      <h2>Inscription</h2>
-      <input type="text"   placeholder="Tapez votre nom" />
-      <br />
-      <input type="text"   placeholder="Tapez votre prénom" />
-      <br />
-      <input type="text"  placeholder="Tapez votre téléphone" />
-      <br />
-      <input type="text"   placeholder="Tapez votre ville" />
-      <br />
-      <Link to={"/"}><button  className="btn btn-outline-success text-uppercase mb-3 px-5"> valider</button></Link>
-     </form>
-
+    <form action="/action_page.php" style={{left:"32%",top:"40%",position:"relative"}}>
+      <div className="mb-3 mt-5" style={{width:"40%"}}>
+        <label  className="form-label">Nom :</label>
+        <input type="text" className="form-control" id="email" placeholder="Entrez votre Nom" />
+      </div>
+      <div className="mb-3 mt-3" style={{width:"40%"}}>
+        <label  className="form-label">Prenom :</label>
+        <input type="text" className="form-control" id="email" placeholder="Entrez votre Prenom" />
+      </div>
+      <div className="mb-3 mt-3" style={{width:"40%"}}>
+        <label  className="form-label">Telephone :</label>
+        <input type="text" className="form-control" id="email" placeholder="Entrez votre NTelephoneom" />
+      </div>
+      <div className="mb-3 mt-3" style={{width:"40%"}} >
+        <label  className="form-label">Ville :</label>
+        <input type="text" className="form-control" id="email" placeholder="Entrez votre Ville" />
+      </div>
+      <div className="mb-3" style={{width:"40%"}}>
+        <Link to={"/"}><button style={{left:"20%",top:"40%",position:"relative"}} className="btn btn-outline-success text-uppercase mb-3 px-5">Valider</button></Link>
+      </div>
+    </form>
   );
 }
 
